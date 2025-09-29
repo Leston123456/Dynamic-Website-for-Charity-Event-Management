f48b529e51001548d4d686973bdcb59bc52197c0
Leston123456 <2115537494@qq.com>
2025-09-29 17:04:24 +0800

    Initial project setup: created directory structure, package.json, and README

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  database: {
+    host: '192.168.153.130',
+    user: 'root',
+    password: '123',
+    database: 'charityevents_db'
+  },
+  server: {
+    port: 3000
+  }
+};
